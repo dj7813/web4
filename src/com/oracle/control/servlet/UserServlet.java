2c59c256e2a1a7a5b55de37e9d9e09f5c382e7d1
tengsir <TengSir@live.cn>
2018-06-21 14:40:27 +0800

    第一次导入

---
+++ src/com/oracle/control/servlet/UserServlet.java
@@ -0,0 +1,20 @@
+package com.oracle.control.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UserServlet")
+public class UserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("test servlet");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        doPost(request,response);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,18 @@
+<%-- Created by IntelliJ IDEA. --%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+  test index jsp
+
+
+  version  control  system (版本管理软件)
+  svn
+  github
+  gitlab
+
+  </body>
+</html>
